2de3668479657ad4e032543b2baa083bbf5c8ddc
Jan Dvorak <dvorakj@gmail.com>
2021-11-01 13:47:09 +0100

    feat: kill previously started process first

--- routes/open.js
+++ routes/open.js
@@ -1,24 +1,40 @@
 var express = require('express');
 var router = express.Router();
-var { exec } = require('child_process');
+var { spawn } = require('child_process');
+
+let chromium
 
 router.post('/', function(req, res, next) {
 
   const url = req.body.url
-  const command = `/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk ${url} &`
+
+  const bin = `/usr/bin/chromium-browser`
+  const params = ['--noerrdialogs', '--disable-infobars', `--kiosk ${url}`]
+  const command = bin + ' ' + params.join(' ')
+
+  if (chromium) {
+    console.log('Stopping')
+    chromium.kill('SIGKILL');
+  }
   
   console.log('Running', command)
-  exec(command, (err, stdout, stderr) => {
-    if (err) {
-      res.render('open', { error });
-      console.error(err)
-    } else {
-      res.render('open', { url });
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    }
+  chromium = spawn(command, params)
+  //chromium = spawn('sleep', ['10'])
+
+  chromium.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  
+  chromium.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+  
+  chromium.on('close', (code) => {
+    console.log(`chromium process exited with code ${code}`);
+    !res.headersSent && res.render('open', { command, code })
   });
 
+  setTimeout(() => res.render('open', { command, code: 'timeout' }), 1000)
 });
 
 module.exports = router;
