5da5c3098b830e49c727c3ecfebd64bd3b9e3b85
Jan Dvořák <dvorakjan@users.noreply.github.com>
2022-02-21 10:38:06 +0100

    feat: end meeting

--- lib.js
+++ lib.js
@@ -3,45 +3,50 @@ var { spawn } = require('child_process');
 var chromium
 
 module.exports = {
-  open: function open(url, callback) {
-    try {
-      const bin = `/usr/bin/chromium-browser`
-      const params = ['--noerrdialogs', '--disable-infobars', '--kiosk', '--no-sandbox', url]
-      const command = bin + ' ' + params.join(' ')
-      let errorSent = false
+  open: open,
+  reset: function(callback) {
+    open('http://localhost:3000/instructions', callback)
+  }
+}
 
-      if (chromium) {
-        console.log('Stopping')
-        chromium.kill('SIGKILL');
-      }
-      
-      console.log('Running', command)
-      chromium = spawn(bin, params)
-      //chromium = spawn('sleep', ['10'])
+function open(url, callback) {
+  try {
+    const bin = `/usr/bin/chromium-browser`
+    const params = ['--noerrdialogs', '--disable-infobars', '--kiosk', '--no-sandbox', url]
+    const command = bin + ' ' + params.join(' ')
+    let errorSent = false
 
-      chromium.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-      });
-
-      chromium.on('error', function(err) {
-        console.error('spawn error', err)
-        errorSent = true
-        callback(err)
-      });
-      
-      chromium.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-      
-      chromium.on('close', (code) => {
-        console.log(`chromium process exited with code ${code}`);
-        !errorSent && callback(null, 'open', { command, code })
-
-        setTimeout(() => callback(null, 'open', { command, code: 'timeout' }), 1000)
-      });
-    } catch (err) {
-      console.error('open error', err)
-      callback(err)
+    if (chromium) {
+      console.log('Stopping')
+      chromium.kill('SIGKILL');
     }
+    
+    console.log('Running', command)
+    chromium = spawn(bin, params)
+    //chromium = spawn('sleep', ['1'])
+
+    chromium.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    chromium.on('error', function(err) {
+      console.error('spawn error', err)
+      errorSent = true
+      callback(err)
+    });
+    
+    chromium.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+    
+    chromium.on('close', (code) => {
+      console.log(`chromium process exited with code ${code}`);
+      !errorSent && callback(null, 'open', { command, code })
+
+      setTimeout(() => callback(null, 'open', { command, code: 'timeout' }), 1000)
+    });
+  } catch (err) {
+    console.error('open error', err)
+    callback(err)
   }
 }
--- routes/open.js
+++ routes/open.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var { open } = require('../lib')
+var { open, reset } = require('../lib')
 
 router.post('/', function(req, res, next) {
   const url = req.body.url
   open(url, (err, params) => {
     !res.headersSent && res.render(err ? 'error' : 'open', {error: err, ...params})
+  })
+});
+
+router.get('/stop', function(req, res, next) {
+  reset(() => {
+    !res.headersSent && res.render('index')
   })
 });
 
